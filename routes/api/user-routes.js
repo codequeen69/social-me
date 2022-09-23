3b9a8e263907f732fba52d5da0d537cee60b2538
Venus DM <crhowe90@gmail.com>
2022-09-23 16:13:31 -0500

    user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,26 @@
+
+const router = require('express').Router();
+const {
+    getAllUser,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller');
+
+
+//set the Get all and Post at /api/users
+router
+.route('/')
+.get(getAllUser)
+.post(createUser)
+
+//set GET one, PUT, and DELETE at /api/user/:id
+
+router
+.route(':id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
+
+module.exports = router;
